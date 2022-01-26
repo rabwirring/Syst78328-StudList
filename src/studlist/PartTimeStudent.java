e8f3a8ee78adb8bfd999af6e182e4ee7b657ef6c
rabwirring <98053589+rabwirring@users.noreply.github.com>
2022-01-26 14:40:37 -0500

    Update PartTimeStudent.java
    inheritence updated

--- src/studlist/PartTimeStudent.java
+++ src/studlist/PartTimeStudent.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package studlist extends Student;
+package studlist;
 
 /**
  *
  * @author Rabjeet singh
  */
-public class PartTimeStudent 
+public class PartTimeStudent extends Student 
 {
     private int numOfCourses;
 
