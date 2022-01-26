7247ddfe891159362769b8ff49dc6661569d8763
rabwirring <wirringr@sheridancollege.ca>
2022-01-26 14:37:29 -0500

    Add part time student

---
+++ src/studlist/PartTimeStudent.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studlist;
+
+/**
+ *
+ * @author Rabjeet singh
+ */
+public class PartTimeStudent 
+{
+    private int numOfCourses;
+
+    /**
+     * @return the numOfCourses
+     */
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    /**
+     * @param numOfCourses the numOfCourses to set
+     */
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+}
